39ac349a45b53ca374f549b97cbc34854a75624c
Khaas01 <kghaas01@gmail.com>
2024-11-20 13:30:15 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -36,37 +36,7 @@ const sections = [
     'solar-detach-reset-section'
 ];
 
-function nextSection() {
-    const projectType = document.querySelector('input[name="projectType"]:checked')?.value;
-    const selectedCompany = document.getElementById('companyName').value;
 
-    switch (sections[currentSection]) {
-        case 'salesRepSection':
-            currentSection = sections.indexOf('companySection');
-            break;
-        case 'companySection':
-            if (selectedCompany === 'Iron Peak Roofing') {
-                currentSection = sections.indexOf('propertyOwnerSection');
-            } else {
-                currentSection = sections.indexOf('someOtherSection'); // Adjust based on your sections
-            }
-            break;
-        case 'propertyOwnerSection':
-            currentSection = sections.indexOf('projectTypeSection');
-            break;
-        case 'projectTypeSection':
-            if (projectType === 'Cash' || projectType === 'Finance') {
-                currentSection = sections.indexOf('roofingTypeSection');
-            } else if (projectType === 'Insurance') {
-                currentSection = sections.indexOf('insuranceInfoSection');
-            } else {
-                currentSection++;
-            }
-            break;
-        default:
-            currentSection++;
-            break;
-    }
 
     showSection(currentSection);
     scrollToTop();
