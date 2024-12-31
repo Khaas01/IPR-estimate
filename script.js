e0db4575e24ac36e5fcd38e7efc9abca94cc8b54
Khaas01 <kghaas01@gmail.com>
2024-12-31 15:24:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Part 1: Core Navigation and Section Management
 
 // Constants
-const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby5M6X1nIlX5saf4Tk7FB2ISwXRNpJVUg9nMueexhjVxse8wCLSI36eUixomDaEOF-ynA/exec'; // Add your deployment URL here
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwafUt0xtsyUlP1vfVgmFitu4LKk4Ebf9BOARExYjb8-YoCnl6tJoQ4pNdVtrR-OBcn/exec'; // Add your deployment URL here
 let currentSection = 'salesRepSection';
 const sectionHistory = [currentSection];
 
