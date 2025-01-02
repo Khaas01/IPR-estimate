6732f21fe74e40bcfd07a3a8b7fd9dde4c42dc4c
Khaas01 <kghaas01@gmail.com>
2025-01-02 12:05:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function updateSignInStatus(isSignedIn) {
   }
 }
 // Constants
-const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby9ap_pg8nPij3LHo5LJPqvxjG-_0AQr3pYGRFMFo1UWrp3GWt_3c863mxsxo74_oRE_g/exec'; // Add your deployment URL here
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw7X5KQvZe_M3i30mHZLNOsZX87r_mcqAio48Ik1kztAa7UA6HEKOM9dnIppOiyCF5uWQ/exec'; // Add your deployment URL here
 let currentSection = 'salesRepSection';
 const sectionHistory = [currentSection];
 
