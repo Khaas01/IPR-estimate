3dfa3398088eb6014131ec875f86e09930326927
Khaas01 <kghaas01@gmail.com>
2025-01-04 16:15:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let gisInited = false;
 let sectionHistory = []; // Initialize sectionHistory
 
 // API configuration constants
-const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzQHetZcUUuqpYhTeTfXH1csQuG1PvYCP5U7GYTJLFVPRjnxQdXHObTylNFhSOF3SZGDQ/exec';
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzbaV5s7msPBnMoG3pdoadPuRIMFS7GAnfxZscwwi4uP1aresQVWifM5bq6OJdNivO9SA/exec';
 const API_KEY = 'AIzaSyDFVaRrTxOyR-fX3XAOp1tjoeg58mkj254';
 const CLIENT_ID = '900437232674-krleqgjop3u7cl4sggmo20rkmrsl5vh5.apps.googleusercontent.com';
 const REDIRECT_URI = 'https://khaas01.github.io'; 
