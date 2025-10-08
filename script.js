0b996517f073f87f9a41d67ac56a6c8e7d427ac8
Khaas01 <kghaas01@gmail.com>
2025-10-07 20:11:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const SHEET_ID = "1fM11c84e-D01z3hbpjLLl2nRaL2grTkDEl5iGsJDLPw";
 const SHEET_NAME = "Form Responses";
 
 const API_CONFIG = {
-  GOOGLE_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbx3gYOKPh7QYG6eVILEtUv7pE9QVG5BOjtDkVv2sn7_ujgQbVeYnoyOVR7egWB5YNePNw/exec',
+  GOOGLE_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbysgDKV5-EIy75ZxUP4zhLoqNq8qu-FbCe-52pZ27wBHoYCegYF4lqTjO5giddZcce55Q/exec',
   API_KEY: 'AIzaSyDFVaRrTxOyR-fX3XAOp1tjoeg58mkj254',
   CLIENT_ID: '900437232674-krleqgjop3u7cl4sggmo20rkmrsl5vh5.apps.googleusercontent.com',
   REDIRECT_URI: 'https://khaas01.github.io/IPR-estimate/',
